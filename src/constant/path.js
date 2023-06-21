0d73b127b0e7e86d4a36e39ca1b299a345e606ed
kateiscoding <kiaksh717@gmail.com>
2023-06-21 18:52:15 +0900

    chore: make constant

---
+++ src/constant/path.js
@@ -0,0 +1,19 @@
+export default class Paths {
+static naver = 'www.naver.com';
+
+static main = '/main';
+
+static intro = '/intro';
+
+static member = '/member';
+
+static notice = '/community';
+
+static resource = '/community/resource';
+
+static achievement = '/business';
+
+static international = '/business/international';
+
+static industrial = '/business/industrial';
+}
---
+++ src/styles/fonts.js
@@ -0,0 +1,70 @@
+export default class Fonts {
+    /* 300 */
+    static light12 = 'font-size:1.2rem; font-weight:300;';
+
+    static light14 = 'font-size:1.4rem; font-weight:300;';
+
+    /* 400 */
+    static regular10 = 'font-size:1rem; font-weight:400;';
+
+    static regular12 = 'font-size:1.2rem; font-weight:400;';
+
+    static regular14 = 'font-size:1.4rem; font-weight:400;';
+
+    static regular18 = 'font-size:1.8rem; font-weight:400;';
+
+    static regular16 = 'font-size:1.6rem; font-weight:400;';
+
+    static regular20 = 'font-size:2rem; font-weight:400;';
+
+    static regular24 = 'font-size:2.4rem; font-weight:400;';
+
+    static regular30 = 'font-size:3rem; font-weight:400;';
+
+    /* 500 */
+    static medium12 = 'font-size:1.2rem; font-weight:500;';
+
+    static medium14 = 'font-size:1.4rem; font-weight:500;';
+
+    static medium16 = 'font-size:1.6rem; font-weight:500;';
+
+    static medium18 = 'font-size:1.8rem; font-weight:500;';
+
+    static medium24 = 'font-size:2.4rem; font-weight:500;';
+
+    /* 600 */
+    static semibold12 = 'font-size:1.2rem; font-weight:600;';
+
+    static semibold14 = 'font-size:1.4rem; font-weight:600;';
+
+    static semibold16 = 'font-size:1.6rem; font-weight:600;';
+
+    static semibold18 = 'font-size:1.8rem; font-weight:600;';
+
+    static semibold20 = 'font-size:2rem; font-weight:600;';
+
+    /* 700 */
+    static bold12 = 'font-size:1.2rem; font-weight:700;';
+
+    static bold14 = 'font-size:1.4rem; font-weight:700;';
+
+    static bold16 = 'font-size:1.6rem; font-weight:700;';
+
+    static bold18 = 'font-size:1.8rem; font-weight:700;';
+
+    static bold20 = 'font-size:2rem; font-weight:700;';
+
+    static bold24 = 'font-size:2.4rem; font-weight:700;';
+
+    static bold26 = 'font-size:2.6rem; font-weight:700;';
+
+    static bold28 = 'font-size:2.8rem; font-weight:700;';
+
+    static bold30 = 'font-size:3rem; font-weight:700;';
+
+    static bold32 = 'font-size:3.2rem; font-weight:700;';
+
+    static bold40 = 'font-size:4rem; font-weight:700;';
+
+    static bold48 = 'font-size:4.8rem; font-weight:700;';
+}
