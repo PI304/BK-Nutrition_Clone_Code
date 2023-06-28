21e59edd03702faa95ecfa548b68d45ab487ece0
kateiscoding <kiaksh717@gmail.com>
2023-06-28 15:01:52 +0900

    feat: router connect

--- src/App.js
+++ src/App.js
@@ -1,13 +1,36 @@
 import './styles/reset.css';
+import {
+	BrowserRouter as Router,
+	Routes,
+	Route,
+} from 'react-router-dom';
 import Main from './pages/main';
+import Achievement from './pages/achievement';
+import Paths from './constant/path';
 import Intro from './pages/intro';
+import Member from './pages/member';
+import Notice from './pages/notice';
+import Resource from './pages/resource';
+import International from './pages/international';
+import Industrial from './pages/industrial';
 
 function App() {
-	return (
-		<div>
-			<Main />
-		</div>
-	);
+    return (
+        <Router>
+            <Routes>
+			<Route path={Paths.main} element={<Main />} />
+			<Route path={Paths.intro} element={<Intro />} />
+			<Route path={Paths.member} element={<Member />} />
+			<Route path={Paths.notice} element={<Notice />} /> 
+			<Route path={Paths.resource} element={<Resource />} /> 
+			<Route path={Paths.achievement} element={<Achievement />} />
+			<Route path={Paths.international} element={<International />} />
+			<Route path={Paths.industrial} element={<Industrial />} />
+			
+            </Routes>
+        </Router>
+
+    );
 }
 
 export default App;
