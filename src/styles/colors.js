7c6cc959397d2ce1e1536cfda9826eb9879da07a
051hui <sheekang30302@gmail.com>
2023-06-30 02:24:46 +0900

    feat: colors

---
+++ src/styles/colors.js
@@ -0,0 +1,53 @@
+export default class Colors {
+	static white = '#FFFFFF';
+
+	static black = '#000000';
+
+	/* gray */
+
+	static gray = '#E4E9F0';
+
+	static gray100 = '#EDEEF3';
+
+	static gray150 = '#D9D9D9';
+
+	static gray200 = '#E4E7EE';
+
+	static gray300 = '#D6DAE4';
+
+	static gray400 = '#C0C4CF';
+
+	static gray500 = '#ADB3C1';
+
+	static gray600 = '#9DA4B5';
+
+	static gray700 = '#8D95A8';
+
+	static gray800 = '#6B748B';
+
+	static gray900 = '#383C46';
+
+	static gray950 = '#2E3139';
+
+	/* blue */
+
+	static blue100 = '#E9F1FB';
+
+	static blue200 = '#D4E3F4';
+
+	static blue300 = '#B4CEED';
+
+	static blue400 = '#7DA8DB';
+
+	static blue500 = '#4786D0';
+
+	static blue600 = '#1B5CA8';
+
+	static blue700 = '#003777';
+
+	static blue800 = '#03162C';
+
+	/* line */
+
+	static line = '#F3F3F3';
+}
