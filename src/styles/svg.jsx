fa8568138f9244a13fdd596254a5f21df394d9f6
Kate <120708446+kateiscoding@users.noreply.github.com>
2023-07-24 15:59:40 +0900

    style: added carousel btn svg

---
+++ src/styles/svg.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function CarouselBtn({ active }) {
+	return (
+		<svg xmlns='http://www.w3.org/2000/svg' width='10' height='11' viewBox='0 0 10 11'>
+			<path
+				d='M9.5 5.12898C9.5 7.61304 7.48546 9.6271 5 9.6271C2.51454 9.6271 0.5 7.61304 0.5 5.12898C0.5 2.64491 2.51454 0.630859 5 0.630859C7.48546 0.630859 9.5 2.64491 9.5 5.12898Z'
+				fill={active ? 'black' : 'white'}
+				stroke='black'
+			/>
+		</svg>
+	);
+}
+
+export default CarouselBtn;
