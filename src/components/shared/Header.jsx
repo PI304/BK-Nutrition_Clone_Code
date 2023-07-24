4bb76e67159bd95d42a23276fe1a53f36aa7d513
Kate <120708446+kateiscoding@users.noreply.github.com>
2023-07-24 16:00:03 +0900

    style: modified header css

--- src/components/shared/Header.jsx
+++ src/components/shared/Header.jsx
@@ -25,10 +25,7 @@ function Header() {
 						<img src={mainLogo} alt='main-logo' />
 					</Link>
 				</div>
-				<div
-					className='dropdown'
-					onMouseEnter={handleDropdownEnter}
-					onMouseLeave={handleDropdownLeave}>
+				<div onMouseEnter={handleDropdownEnter} onMouseLeave={handleDropdownLeave}>
 					<div>
 						<Link to={Paths.intro}>교육팀 소개</Link>
 					</div>
@@ -41,87 +38,39 @@ function Header() {
 					<div>
 						<Link to={Paths.notice}>커뮤니티</Link>
 					</div>
-					<div
-						className={`dropdown-content ${showDropdown ? 'show' : ''}`}
-						onMouseEnter={handleDropdownEnter}
-						onMouseLeave={handleDropdownLeave}>
+				</div>
+			</div>
+			{showDropdown && (
+				<Dropdown onMouseEnter={handleDropdownEnter} onMouseLeave={handleDropdownLeave}>
+					<div>
 						<div>
 							<div>
 								<Link to={Paths.intro}>인사말</Link>
 								<Link to={Paths.intro}>목표 및 비전</Link>
 								<Link to={Paths.intro}>조직 현황</Link>
 								<Link to={Paths.intro}>찾아오시는 길</Link>
+								<div />
 							</div>
 							<div>
 								<Link to={Paths.member}>참여교수</Link>
 								<Link to={Paths.member}>신진 연구인력</Link>
 								<Link to={Paths.member}>참여 대학원생</Link>
-								<div className='grid-divider' />
+								<div />
 							</div>
 							<div>
 								<Link to={Paths.achievement}>연구 업적</Link>
 								<Link to={Paths.international}>국제 협력</Link>
 								<Link to={Paths.industrial}>산학 협력</Link>
-								<div className='grid-divider' />
+								<div />
 							</div>
 							<div>
 								<Link to={Paths.notice}>공지사항</Link>
 								<Link to={Paths.resource}>자료실</Link>
 							</div>
 						</div>
-
-						{/* <div className='grid-container'>
-							<div className='grid-column'>
-								<Link to={Paths.intro} className='grid-item'>
-									인사말
-								</Link>
-								<Link to={Paths.intro} className='grid-item'>
-									목표 및 비전
-								</Link>
-								<Link to={Paths.intro} className='grid-item'>
-									조직 현황
-								</Link>
-								<Link to={Paths.intro} className='grid-item'>
-									찾아오시는 길
-								</Link>
-								<div className='grid-divider' />
-							</div>
-							<div className='grid-column'>
-								<Link to={Paths.member} className='grid-item'>
-									참여교수
-								</Link>
-								<Link to={Paths.member} className='grid-item'>
-									신진 연구인력
-								</Link>
-								<Link to={Paths.member} className='grid-item'>
-									참여 대학원생
-								</Link>
-								<div className='grid-divider' />
-							</div>
-							<div className='grid-column'>
-								<Link to={Paths.achievement} className='grid-item'>
-									연구 업적
-								</Link>
-								<Link to={Paths.international} className='grid-item'>
-									국제 협력
-								</Link>
-								<Link to={Paths.industrial} className='grid-item'>
-									산학 협력
-								</Link>
-								<div className='grid-divider' />
-							</div>
-							<div className='grid-column'>
-								<Link to={Paths.notice} className='grid-item'>
-									공지사항
-								</Link>
-								<Link to={Paths.resource} className='grid-item'>
-									자료실
-								</Link>
-							</div>
-						</div> */}
 					</div>
-				</div>
-			</div>
+				</Dropdown>
+			)}
 		</HeaderContainer>
 	);
 }
@@ -130,7 +79,7 @@ const HeaderContainer = styled.header`
 	display: flex;
 	justify-content: center;
 
-	> div {
+	> div:first-child {
 		width: 120rem;
 		display: flex;
 		justify-content: space-between;
@@ -141,25 +90,23 @@ const HeaderContainer = styled.header`
 			display: flex;
 
 			> a {
-				
 				> img {
 					object-fit: contain;
 					width: 27rem;
 				}
-
 			}
 		}
 
-		> div:last-child {
+		> div:nth-child(2) {
 			display: flex;
 			flex-wrap: nowrap;
-			gap: 6rem;
+			gap: 1.6rem;
 			// width: 65rem;
 
 			> div {
 				// width: 100%;
+				width: 13.3rem;
 				height: 100%;
-				// margin: 1.6rem;
 				cursor: pointer;
 
 				> a {
@@ -171,102 +118,66 @@ const HeaderContainer = styled.header`
 					${Fonts.bold20}
 				}
 			}
-
-			> div:last-child {
-				// .dropdown-content 입니다
-				// display: none;
-				position: absolute;
-				background: rgba(0, 0, 0, 0.55);
-				width: 100%;
-				// width: 100%으로 주니 안 맞음
-				box-shadow: 0px 0.8rem 1rem 0px rgba(0, 0, 0, 0.2);
-				z-index: 1;
-				top: 9rem;
-				color: white;
-				transform: translateX(-52%);
-				height: 13rem;
-
-				> div {
-		
-					display: flex;
-
-					gap:5rem;
-					padding: 1rem;
-					width: 40%;
-					transform: translateX(120%);
-
-
-					> div {
-						// .grid-column 
-						display: flex;
-						flex-direction: column;
-						position: relative;
-						align-items: center;
-					
-					> 	div {
-						position: absolute; /* 절대 위치 설정 */
-						right: 0;
-						top: 50%;
-						transform: translateY(-50%);
-						width: 0.1rem; /* 가로 선의 너비 */
-						height: 90%; /* 가로 선의 높이 */
-						background-color: white; /* 가로 선의 색상 */
-					}
-
-					> a .grid-item {
-						color: white;
-						display: flex;
-						align-items: center;
-						justify-content: center;
-						font-size: 1.4rem;
-						font-family: 'Inter';
-						font-style: normal;
-						font-weight: 500;
-						line-height: 1.7rem;
-						padding: 0.5rem 0.5rem;
-					}
-					
-					}
-
-
-					}
-				}
-			}
 		}
 	}
 `;
 
-const HeaderLayout = styled.div`
-	.dropdown-content.show {
-		display: block;
-	}
+const Dropdown = styled.div` 
+position: absolute;
+background: rgba(0, 0, 0, 0.55);
+width: 100%;
+box-shadow: 0px 0.8rem 1rem 0px rgba(0, 0, 0, 0.2);
+z-index: 1;
+top: 9rem;
+color: white;
+// transform: translateX(-0%);
+height: 13rem;
+display: flex;
+justify-content: center;
 
-	// .grid-container {
-	// 	display: grid;
-	// 	grid-template-columns: repeat(4, 0.5fr);
-	// 	padding: 1rem;
-	// 	width: 40%;
-	// 	transform: translateX(120%);
-	// }
+> div {
+    width: 120rem;
+    height: 13rem;
+    display: flex;
+    justify-content: flex-end;
 
-	// .grid-column {
-	// 	display: flex;
-	// 	flex-direction: column;
-	// 	position: relative;
-	// }
+    > div {
+        display: flex;
+        width: 58rem;
+        gap: 1.6rem;
+        position: absolute;
+        padding: 1.6rem 0rem;
+        justify-content: center;
 
-	// .grid-item {
-	// 	color: white;
-	// 	display: flex;
-	// 	align-items: center;
-	// 	justify-content: center;
-	// 	font-size: 1.4rem;
-	// 	font-family: 'Inter';
-	// 	font-style: normal;
-	// 	font-weight: 500;
-	// 	line-height: 1.7rem;
-	// 	padding: 0.5rem 0.5rem;
-	// }
+        > div {
+            display: flex;
+            flex-direction: column;
+            position: relative;
+            align-items: center;
+            gap: 1rem;
+            width: 13rem;
+                    
+                    >   div: last-child {
+                        position: absolute;
+                        right: 3rem;
+                        transform: translatex(4rem);
+                        width: 0.05rem; 
+                        height: 10rem;
+                        background-color: white; 
+                    }
+
+                    > a {
+                        color: white;
+                        display: flex;
+                        align-items: center;
+                        justify-content: center;
+                        line-height: 1.7rem;
+                        ${Fonts.medium14};
+                    }                   
+                    }
+                }
+            }
+        }
 `;
 
 export default Header;
